b5ca9b31c1f07e042940a53bcffd267ac223c67c
zakaria khaine <33629767+zskthecodeman@users.noreply.github.com>
2017-11-13 17:26:02 +0100

    #1

--- document.php
+++ document.php
@@ -1,8 +1,16 @@
 <?php
 include 'template/header.php';
 ?>
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                1 of 2
+            </div>
+            <div class="col">
+                1 of 2
+            </div>
+        </div>
+    </div>
 <?php
 include 'template/footer.php';
 ?>
