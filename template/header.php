260f53bb5ab6feb0564e6b613ebf12f546f2604d
Valentin-Genauzeau <vgenauzeau@gmail.com>
2017-11-13 18:07:53 +0100

    #1

--- template/header.php
+++ template/header.php
@@ -16,33 +16,38 @@
 
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Portail Léonard de Vinci</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tableau de bord<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
+                        <a class="nav-link" href="#">Office 365</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link" href="#">Planning général</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Promotion école</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Learning Center</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ressource Info.</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inscription sport</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Occupation des salles</a>
+                    </li>
+
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
