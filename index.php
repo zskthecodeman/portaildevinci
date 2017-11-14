32e4c544711ecbc651b6b9119e1292915480a7a3
Valentin-Genauzeau <vgenauzeau@gmail.com>
2017-11-14 01:19:15 +0100

    1

--- index.php
+++ index.php
@@ -2,32 +2,68 @@
 
     <div class="row">
         <div class="col-md-12 background">
+
             <div class="container-fluid">
                 <div class="row">
+
                     <div class="col-md-12">
+
                         <h1>Tableau de bord Étudiant</h1>
                         <div class="white">
                             <h7>Mon Tableau de bord</h7>
                         </div>
-                        <br>
-                        <nav>
+
+                        <nav class="lgscreen">
                             <ul class="navbar-nav mr-auto align">
-                                <li class="nav-item"><button>Fiche Étudiant</button></li>
-                                <li class="nav-item align-baseline"><button>Mon E.D.T</button></li>
-                                <li class="nav-item"><button>Mes Absences</button></li>
-                                <li class="nav-item"><button>Mes notes</button></li>
-                                <li class="nav-item"><button>Mes cours</button></li>
-                                <li class="nav-item"><button>Documents</button></li>
-                                <li class="nav-item"><button>Stages</button></li>
-                                <li class="nav-item"><button>International</button></li>
-                                <li class="nav-item"><button>Rendus pédagogique</button></li>
-                                <li class="nav-item"><button>Finances</button></li>
-                                <li class="nav-item"><button>Promotion école</button></li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Fiche Étudiant</a>
+                                </li>
+                                <li class="nav-item align-baseline">
+                                    <a href="#" class="btn btn-effect">Mon E.D.T</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Mes Absences</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Mes notes</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Mes cours</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Documents</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Stages</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">International</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Rendus pédagogique</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Finances</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="btn btn-effect">Promotion école</a>
+                                </li>
                             </ul>
                         </nav>
+
+                        <nav class="smscreen">
+                            <div class="smallmenu">
+                                <div class="">
+                                    <i class="fa fa-chevron-circle-down fa-3x clic " aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </nav>
+
                     </div>
+
                 </div>
             </div>
+
         </div>
     </div>
 
--- template/header.php
+++ template/header.php
@@ -8,6 +8,8 @@
 
     <title>Portail Léonard de Vinci</title>
 
+    <link rel="stylesheet" href="javascript.js" type="text/javascript">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
