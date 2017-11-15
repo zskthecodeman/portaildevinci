e43f032bc4dd883122ad82e1c23737e1f8175774
zakaria khaine <33629767+zskthecodeman@users.noreply.github.com>
2017-11-15 14:34:36 +0100

    #1

--- document.php
+++ document.php
@@ -4,7 +4,46 @@ include 'template/header.php';
     <div class="container">
         <div class="row">
             <div class="col first-box">
-                1 of 2
+                <header>
+                    <h4>1 of 2</h4>
+                    <div class=body-box>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                        <h4>SCOLARITE</h4>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                        <h4>STAGES</h4>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                        <h4>GENERAL</h4>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                        <h4>VIE ASSOCIATIVE</h4>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                        <h4>SPORT</h4>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <p><a><i></i></a></p>
+                        <hr>
+                    </div>
+                </header>
             </div>
             <div class="col">
                 1 of 2
